a117d01bd8c1ba2f5e484a2ff43250a85a1e7de6
Grant Hutchinson <grant@randomairborne.dev>
2022-04-14 16:09:17 +0000

    update to use content-types library

--- README.md
+++ README.md
@@ -6,15 +6,16 @@ axum_static_macro = "1"
 This package has a single macro (static_file) which takes arguments for name, file path, and content type and fills out the proper statics. \
 In debug mode, it will read the file live so you can change it without recompiling the program. (Works only in crate root.) \
 It takes three arguments. Function name (this is what you wrap in the axum get handler), file path, and content type. \
-Does not panic in release mode. Debug mode can panic if the file does not exist.
+Does not panic in release mode. Debug mode can panic if the file does not exist. \
+Includes a module (content_types) with consts for common content types.
 
 ```rust
 #[tokio::main]
 async fn main() {
     // create our static file handler
-    axum_static_macro::static_file!(static_root, "index.html", axum_static_macro::content_types::HTML);
+    axum_static_macro::static_file!(index, "index.html", axum_static_macro::content_types::HTML);
     // build our application with a single route
-    let app = axum::Router::new().route("/", axum::routing::get(static_root));
+    let app = axum::Router::new().route("/", axum::routing::get(index));
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@
 //! In debug mode, it will read the file live so you can change it without recompiling the program. Note: You must be in the crate root for this to work.
 //! It takes three arguments. Function name (this is what you wrap in the axum get handler), file path, and content type.
 //! Does not panic in release mode. Debug mode can panic if the file does not exist.
+//! Includes the content_types module to simplify returning static files of certain types.
+//!
 //! ```rust
 //! #[tokio::main]
 //! async fn main() {
-//!     axum_static_macro::static_file!(static, "index.html", "text/html");
+//!     axum_static_macro::static_file!(index, "index.html", axum_static_macro::content_types::HTML);
 //!     // build our application with a single route
-//!     let app = axum::Router::new().route("/", axum::routing::get(static));
+//!     let app = axum::Router::new().route("/", axum::routing::get(index));
 //!     // run it with hyper on localhost:3000
 //!     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
 //!         .serve(app.into_make_service())
@@ -17,7 +19,7 @@
 //! ```
 
 /// A collection of content MIME types, for use as the third argument of static_file
-pub mod content_types;
+pub use content_types;
 
 /// Usage: `static_file!(root, "index.html", "text/html")`
 
